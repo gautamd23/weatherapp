26912798651daed0c4d37f3ed855aa17501138f5
Gautam Dhiman <gautamdhiman60@gmail.com>
2021-10-16 15:16:34 +0530

    third

--- weth/urls.py
+++ weth/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/',include('app.urls'))
+    path('',views.home, name = 'home')
 ]
